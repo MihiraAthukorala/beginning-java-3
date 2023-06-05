add1eb7cea391fd5c0360ee38af0a1110cb060fa
MihiraAthukorala <mahiathukorala2022@gmail.com>
2023-06-05 12:34:58 +0530

    Subtracting Two Numbers

---
+++ Subtract.java
@@ -0,0 +1,22 @@
+// Key Board Input With Subtracting
+
+import java.util.*;
+
+class Subtract {
+    public static void main(String args[]) {
+
+        System.out.println("The subtraction value of two numbers \n");
+
+        Scanner input = new Scanner(System.in);
+
+        int x,y,z;
+        System.out.print("Input Your 1st Number : ");
+        x=input.nextInt();
+        System.out.print("Input Your 2nd Number : ");
+        y=input.nextInt();
+
+        z=x-y;
+        System.out.print("The value is when you subtract the two numbers you entered : " +z);
+
+    }
+}
