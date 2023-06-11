a2573a1cadd89f201e6cda5985083318ef595d59
MihiraAthukorala <mahiathukorala2022@gmail.com>
2023-06-11 21:52:59 +0530

    substract 11.6.2023

--- Subtract.java
+++ Subtract.java
@@ -1,22 +1,47 @@
 // Key Board Input With Subtracting
 
+// import java.util.*;
+
+// class Subtract {
+//     public static void main(String args[]) {
+
+//         System.out.println("The subtraction value of two numbers \n");
+
+//         Scanner input = new Scanner(System.in);
+
+//         int x,y,z;
+//         System.out.print("Input Your 1st Number : ");
+//         x=input.nextInt();
+//         System.out.print("Input Your 2nd Number : ");
+//         y=input.nextInt();
+
+//         z=x-y;
+//         System.out.print("The value is when you subtract the two numbers you entered : " +z);
+
+//     }
+// }
+
+// Key Board Input With Subtacting
+
 import java.util.*;
 
 class Subtract {
     public static void main(String args[]) {
 
-        System.out.println("The subtraction value of two numbers \n");
+        System.out.println("The Subtraction value of two numbers \n");
 
         Scanner input = new Scanner(System.in);
 
         int x,y,z;
+
         System.out.print("Input Your 1st Number : ");
         x=input.nextInt();
+
         System.out.print("Input Your 2nd Number : ");
         y=input.nextInt();
 
         z=x-y;
-        System.out.print("The value is when you subtract the two numbers you entered : " +z);
+        System.out.print("The value is when you subtract the  two numbers you entered : "+z);
 
     }
 }
